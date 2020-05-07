a304c03635d6ad7717b7dc0ea354baf0ddb94ff4
Kyle Douglas <kyle.douglas@dulwich-beijing.cn>
2020-05-07 12:03:12 +0930

    Added tweaked design, added begin and end fill blocks to index.html, added comment to indicate custom code present

--- generators/python/custom_code.js
+++ generators/python/custom_code.js
@@ -4,6 +4,7 @@ goog.require('Blockly.Python');
 
 Blockly.Python['insert_user_code'] = function(block) {
   var text_user_code = block.getFieldValue('user_code');
+  Blockly.Python.definitions_['custom_code'] = "#You've added your own code";
   var code = text_user_code + '\n';
   return code;
 };
