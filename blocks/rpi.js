75ae7cef198fe89cbab7c1b5fc5d014aef93dcf5
Kyle Douglas <kyle.douglas@dulwich-beijing.cn>
2020-07-19 11:58:51 +0930

    Added Raspberry Pi
    +LED and colour LED blocks

--- blocks/rpi.js
+++ blocks/rpi.js
@@ -8,5 +8,138 @@ goog.require('Blockly.FieldNumber');
 goog.require('Blockly.FieldVariable');
 
 Blockly.defineBlocksWithJsonArray([  // BEGIN JSON EXTRACT
-
+{
+  "type": "rpi_new_led",
+  "message0": "%1 %2 pin: %3",
+  "args0": [
+    {
+      "type": "field_label_serializable",
+      "name": "NAME",
+      "text": "RGB LED:"
+    },
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_value",
+      "name": "PIN",
+      "check": "Number",
+      "align": "RIGHT"
+    }
+  ],
+  "output": "RPI_LED",
+  "colour": 270,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "rpi_new_rgb_led",
+  "message0": "%1 %2 red pin: %3 green pin: %4 blue pin: %5",
+  "args0": [
+    {
+      "type": "field_label_serializable",
+      "name": "NAME",
+      "text": "RGB LED:"
+    },
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_value",
+      "name": "RED_PIN",
+      "check": "Number",
+      "align": "RIGHT"
+    },
+    {
+      "type": "input_value",
+      "name": "GREEN_PIN",
+      "check": "Number",
+      "align": "RIGHT"
+    },
+    {
+      "type": "input_value",
+      "name": "BLUE_PIN",
+      "check": "Number",
+      "align": "RIGHT"
+    }
+  ],
+  "output": "RPI_RGB_LED",
+  "colour": 270,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "rpi_set_rgb_led_colour",
+  "message0": "set RGB LED: %1 %2 to colour %3",
+  "args0": [
+    {
+      "type": "input_dummy"
+    },
+    {
+      "type": "input_value",
+      "name": "LED",
+      "check": "RPI_RGB_LED"
+    },
+    {
+      "type": "input_value",
+      "name": "COLOUR",
+      "check": "Colour",
+      "align": "RIGHT"
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 270,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "rpi_led_on",
+  "message0": "turn LED: %1 on",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "LED",
+      "check": "RPI_LED"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 270,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "rpi_led_off",
+  "message0": "turn LED: %1 off",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "LED",
+      "check": "RPI_LED"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 270,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "rpi_led_toggle",
+  "message0": "toggle/invert LED: %1",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "LED",
+      "check": "RPI_LED"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 270,
+  "tooltip": "",
+  "helpUrl": ""
+}
 ]);  // END JSON EXTRACT (Do not delete this comment.)
--- generators/python/rpi.js
+++ generators/python/rpi.js
@@ -5,6 +5,57 @@ goog.require('Blockly.Extensions');
 
 Blockly.Python.addReservedWords('RPi');
 
-var rpi_imports = 'import RPi.GPIO'
+var rpi_gpio_imports = 'import RPi.GPIO'
+var rpi_gpiozero_imports = 'import gpiozero'
 
-  //Blockly.Python.definitions_['import_rpi_gpio'] = rpi_imports;
+Blockly.Python['rpi_new_led'] = function(block) {
+  Blockly.Python.definitions_['import_gpiozero'] = rpi_gpiozero_imports;
+  var value_pin = Blockly.Python.valueToCode(block, 'PIN', Blockly.Python.ORDER_ATOMIC);
+  var code = 'gpiozero.PWMLED('+value_pin+',active_high=True, initial_value=0, frequency=100, pin_factory=None)';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['rpi_new_rgb_led'] = function(block) {
+  Blockly.Python.definitions_['import_gpiozero'] = rpi_gpiozero_imports;
+  var value_red_pin = Blockly.Python.valueToCode(block, 'RED_PIN', Blockly.Python.ORDER_ATOMIC);
+  var value_green_pin = Blockly.Python.valueToCode(block, 'GREEN_PIN', Blockly.Python.ORDER_ATOMIC);
+  var value_blue_pin = Blockly.Python.valueToCode(block, 'BLUE_PIN', Blockly.Python.ORDER_ATOMIC);
+  var code = 'gpiozero.RGBLED('+value_red_pin+', '+value_green_pin+', '+value_blue_pin+', active_high=True, initial_value=(1, 1, 1), pwm=True, pin_factory=None)';
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+    
+Blockly.Python['rpi_set_rgb_led_colour'] = function(block) {
+  var value_led = Blockly.Python.valueToCode(block, 'LED', Blockly.Python.ORDER_ATOMIC);
+  var value_colour = Blockly.Python.valueToCode(block, 'COLOUR', Blockly.Python.ORDER_ATOMIC);
+  var hexColourFunctionName = Blockly.Python.provideFunction_(
+        'hex_colour_to_tuple',
+        ['def ' + Blockly.Python.FUNCTION_NAME_PLACEHOLDER_ + '(colour_string):',
+         '  try:',
+         '      if len(colour_string) != 7:',
+         '          return (0,0,0)',
+         '      else:',
+         '          return (int(colour_string[1:3],16)/255,int(colour_string[3:5],16)/255,int(colour_string[5:7],16)/255)',
+         '  except:',
+         '      print("Colour error, using black")',
+         '      return (0,0,0)']);
+  var code = value_led + '.color = ' + hexColourFunctionName + '('+value_colour+')\n';
+  return code;
+};
+
+Blockly.Python['rpi_led_on'] = function(block) {
+  var value_led = Blockly.Python.valueToCode(block, 'LED', Blockly.Python.ORDER_ATOMIC);
+  var code = value_led + '.on()\n';
+  return code;
+};
+
+Blockly.Python['rpi_led_off'] = function(block) {
+  var value_led = Blockly.Python.valueToCode(block, 'LED', Blockly.Python.ORDER_ATOMIC);
+  var code = value_led + '.off()\n';
+  return code;
+};
+
+Blockly.Python['rpi_led_toggle'] = function(block) {
+  var value_led = Blockly.Python.valueToCode(block, 'LED', Blockly.Python.ORDER_ATOMIC);
+  var code = value_led + '.toggle()\n';
+  return code;
+};
