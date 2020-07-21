f82482486bdfc51c07f8a79be07ade5ac230a52e
Kyle Douglas <kyle.douglas@dulwich-beijing.cn>
2020-07-21 23:11:49 +0930

    Update rpi.js
    Fixed MotionSensor contructor block code

--- generators/python/rpi.js
+++ generators/python/rpi.js
@@ -260,7 +260,7 @@ Blockly.Python['rpi_usonic_get_distance'] = function(block) {
 Blockly.Python['rpi_new_motion_sensor'] = function(block) {
   Blockly.Python.definitions_['import_gpiozero'] = rpi_gpiozero_imports;
   var value_pin = Blockly.Python.valueToCode(block, 'PIN', Blockly.Python.ORDER_ATOMIC);
-  var code = 'gpiozero.MotionSensor('+value_pin+', *, queue_len=1, sample_rate=10, threshold=0.5, partial=False, pin_factory=None)';
+  var code = 'gpiozero.MotionSensor('+value_pin+', queue_len=1, sample_rate=10, threshold=0.5, partial=False, pin_factory=None)';
   return [code, Blockly.Python.ORDER_NONE];
 };
 
